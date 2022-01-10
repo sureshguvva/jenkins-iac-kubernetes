0cd78fcd16eb2f3018d7057e3b89160513f9f995
KalashPalwanda <kalashp69@gmail.com>
2022-01-10 10:55:27 +0530

    committed

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -19,24 +19,14 @@ yum install docker -y
 #sudo usermod -a -G docker jenkins
 #sudo service docker start
 sudo chkconfig docker on
-
-# echo "Install Jenkins"
-# sudo yum install wget -y
-# sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-# sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-# sudo yum install -y jenkins
-# sudo usermod -a -G docker jenkins
-# sudo chkconfig jenkins on
-# sudo service docker start
-# sudo service jenkins start
-
-# sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-# sudo yum upgrade
-# sudo yum install epel-release java-1.8.0-openjdk
-# sudo yum install jenkins
-# sudo systemctl daemon-reload
-# echo "Start Docker & Jenkins services"
 sudo service docker start
-# sudo service jenkins start
+sudo chmod 666 /var/run/docker.sock
+
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
 
