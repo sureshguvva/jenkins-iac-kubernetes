66631cce120f3b095ca6a13f88c067c2f6f24d3f
KalashPalwanda <kalashp69@gmail.com>
2022-01-10 12:02:38 +0530

    commit

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -23,6 +23,7 @@ sudo service docker start
 sudo chmod 666 /var/run/docker.sock
 
 sudo yum update –y
+sudo yum -y install wget
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade
