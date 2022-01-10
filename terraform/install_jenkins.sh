f1f240a20ae1739d4c751036452b7874441343b3
KalashPalwanda <kalashp69@gmail.com>
2022-01-10 12:27:10 +0530

    commit

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -10,6 +10,14 @@ yum install -y java-11-openjdk-devel
 echo "Install Maven"
 yum install -y maven 
 
+sudo yum install -y jenkins java-11-openjdk-devel
+sudo yum -y install wget
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade -y
+sudo yum install jenkins -y
+sudo systemctl start jenkins
+
 echo "Install git"
 yum install -y git
 
@@ -22,11 +30,5 @@ sudo chkconfig docker on
 sudo service docker start
 sudo chmod 666 /var/run/docker.sock
 
-sudo yum install -y jenkins java-11-openjdk-devel
-sudo yum -y install wget
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade -y
-sudo yum install jenkins -y
-sudo systemctl start jenkins
+
 
