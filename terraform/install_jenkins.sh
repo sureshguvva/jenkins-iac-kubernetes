e96b330fbb1a99bfcb2e670d82f806b84fc296c6
KalashPalwanda <kalashp69@gmail.com>
2022-01-10 12:17:25 +0530

    commit

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -5,7 +5,7 @@ sudo yum -y update
 
 echo "Install Java JDK 8"
 yum remove -y java
-yum install -y java-1.8.0-openjdk
+yum install -y java-11-openjdk-devel
 
 echo "Install Maven"
 yum install -y maven 
@@ -22,12 +22,11 @@ sudo chkconfig docker on
 sudo service docker start
 sudo chmod 666 /var/run/docker.sock
 
-sudo yum update –y
+sudo yum install -y jenkins java-11-openjdk-devel
 sudo yum -y install wget
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo yum install jenkins java-1.8.0-openjdk-devel -y
-sudo systemctl daemon-reload
+sudo yum upgrade -y
+sudo yum install jenkins -y
 sudo systemctl start jenkins
 
