cbb960e9528f0c072bb3724ea67a9da7b1a40e24
arjun225 <p.arjuncrm@gmail.com>
2022-01-19 21:31:35 +0530

    commited

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -27,57 +27,6 @@ data "aws_ami" "amazon-linux-2" {
   }
 }
 
-
-
-resource "aws_instance" "jenkins2-instance" {
-  ami             = "${data.aws_ami.amazon-linux-2.id}"
-  instance_type   = "t2.medium"
-  key_name        = "${var.keyname}"
-  #vpc_id          = "${aws_vpc.development-vpc.id}"
-  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
-  subnet_id          = "${aws_subnet.public-subnet-1.id}"
-  #name            = "${var.name}"
-  user_data = "${file("install_jenkins.sh")}"
-
-  associate_public_ip_address = true
-  tags = {
-    Name = "Jenkins2-Instance"
-  }
-}
-
-resource "aws_security_group" "sg_allow_ssh_jenkins" {
-  name        = "allow_ssh_jenkins"
-  description = "Allow SSH and Jenkins inbound traffic"
-  vpc_id      = "${aws_vpc.development-vpc.id}"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-}
-
-output "jenkins_ip_address" {
-  value = "${aws_instance.jenkins2-instance.public_dns}"
-}
-
-
-
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -1,36 +1,36 @@
 
 
 #!/bin/bash
-sudo yum -y update
+# sudo yum -y update
 
-echo "Install Java JDK 8"
-sudo yum remove -y java
-sudo yum install -y java-1.8.0-openjdk-devel
+#xecho "Install Java JDK 8"
+#sudo yum remove -y java
+#sudo yum install -y java-1.8.0-openjdk-devel
 
-echo "Install Maven"
-sudo yum install -y maven 
+#echo "Install Maven"
+#sudo yum install -y maven 
 
-echo "Install git"
-sudo yum install -y git
+#echo "Install git"
+#sudo yum install -y git
 
-echo "Install Docker engine"
-sudo yum update -y
-sudo yum install docker -y
-sudo usermod -a -G docker jenkins
+#echo "Install Docker engine"
+#sudo yum update -y
+#sudo yum install docker -y
+#sudo usermod -a -G docker jenkins
 #sudo chkconfig docker on
-sudo service docker start
-sudo chmod 666 /var/run/docker.sock
+#sudo service docker start
+#sudo chmod 666 /var/run/docker.sock
 
-sudo amazon-linux-extras install epel -y
-sudo yum update –y
-sudo yum -y install wget
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo yum install jenkins java-1.8.0-openjdk-devel -y
-sudo systemctl daemon-reload
-sudo systemctl start jenkins
-sudo ufw allow 8080
+#sudo amazon-linux-extras install epel -y
+#sudo yum update –y
+#sudo yum -y install wget
+#sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+#sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+#sudo yum upgrade
+#sudo yum install jenkins java-1.8.0-openjdk-devel -y
+#sudo systemctl daemon-reload
+#sudo systemctl start jenkins
+#sudo ufw allow 8080
 
 
 
